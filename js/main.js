9c17e091e63d28dc93fc7e6e377376c243e132ba
Tel <telsabate.business@gmail.com>
2025-04-23 20:39:11 +0800

    Fixed custom and number of people input validation.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,71 @@
-const billInput = document.getElementById( "billInput" );
+const form = document.getElementById("form");
+const inputElemIDs = [ "billInput", "percentageInput", "numberOfPeopleInput" ];
 const billInputError = document.getElementById( "billInputError" );
+const percentageInputError = document.getElementById( "percentageInputError" );
+const numberOfPeopleInputError = document.getElementById( "numberOfPeopleInputError" );
+const errorMsgElements = {
+    billInput: billInputError,
+    percentageInput: percentageInputError,
+    numberOfPeopleInput: numberOfPeopleInputError,
+};
 
-billInput.addEventListener( 'keydown', function(e){ 
+const validityFuncs = {
+    billInput: checkBillValidity,
+    percentageInput: checkPercentageValidity,
+    numberOfPeopleInput: checkNumOfPeopleValidity,
+};
+
+function setupListeners(){
+    inputElemIDs.forEach( (id) => {
+        let elem = document.getElementById( id );
+
+        elem.addEventListener( 'keydown', keydownListener );
+        elem.addEventListener( 'keyup', keyupListener );
+        elem.addEventListener( 'change', checkValidity );
+    });
+
+    const percentageButtons = document.getElementsByName( "percentage" );
+
+    percentageButtons.forEach( (button) => {
+        button.addEventListener( "click", function(e){
+            e.target.parentElement.classList.add( "selected-tip" );
+        });
+    });
+}
+
+function keydownListener(e){
     let value = e.target.value;
-    const key = e.key;
-    const specialAllowedKeys = ["Tab", "Enter", "Backspace"];
-    const notAllowedKeys = [ "-", "+" ];
-
+    const key = e.key, id = e.target.id;
+    const specialAllowedKeys = ["Tab", "Enter", "Backspace", "ArrowLeft", "ArrowRight"];
+    const maxInputLength = {
+        billInput: 6,
+        percentageInput: 3,
+        numberOfPeopleInput: 3,
+    };
+    const notAllowedKeys = [ "-", "+", "." ];
+    if( id == "billInput" ) notAllowedKeys.splice(-1);
+    
     if( !specialAllowedKeys.includes(key) ){
-        // console.log(`value`, value)
-        if( value.length > 6 || notAllowedKeys.includes(key) ) e.preventDefault();
+        if( value.length >= maxInputLength[id] || notAllowedKeys.includes(key) ) e.preventDefault();
     }
-});
+}
 
-billInput.addEventListener( 'keyup', function(e){ 
-    // console.log(`keyup`, e.target.value.length)
+function keyupListener(e){
     const key = e.key;
-
+    // console.log(`keyup`, key)
+    
     if( key != "Tab" && key != "Enter" ){
-        billInputError.innerHTML = "";
+        errorMsgElements[ e.target.id ].innerHTML = "";
     } else {
-        checkBillValidity(e);
+        checkValidity(e);
     }
-});
+}
 
-billInput.addEventListener( 'change', function(e){ 
-    checkBillValidity(e);
-});
-
+function checkValidity(e){
+    if( validityFuncs[ e.target.id ] ){
+        validityFuncs[ e.target.id ](e);
+    }
+}
 
 function checkBillValidity(e){
     let validity = e.target.validity;
@@ -38,3 +76,23 @@ function checkBillValidity(e){
     else if( validity.badInput ) billInputError.innerHTML = "Invalid Amount";
     
 }
+
+function checkPercentageValidity(e){
+    let validity = e.target.validity;
+
+    if( validity.rangeUnderflow || validity.rangeOverflow ) percentageInputError.innerHTML = "Should be 1 - 100";
+    else if( validity.badInput ) percentageInputError.innerHTML = "Invalid Percentage";
+}
+
+function checkNumOfPeopleValidity(e){
+    let validity = e.target.validity;
+
+    // console.log(`checkNumOfPeopleValidity`, validity)
+    if( validity.rangeUnderflow ) numberOfPeopleInputError.innerHTML = "Can't be zero";
+    else if( validity.badInput ) numberOfPeopleInputError.innerHTML = "Invalid Value";
+}
+
+
+
+
+setupListeners();
